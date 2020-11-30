22c7eaba880c8ca406f4bc227b8dc7e4fa64bb70
Anna Tan <annat@Annas-MacBook-Pro.local>
2020-11-30 02:25:35 -0500

    move addCourse to new file

--- grabCore.py
+++ grabCore.py
@@ -42,30 +42,6 @@ def grabNumber():
     print('Major: %s'%programs[major-1])
     return major
 
-def addCourse(course_dict,maxcred=54):
-    add=input('Do you wanna add a class? (Y/N): ')
-    units=0
-    courses=[]
-    print(courses)
-    if add.upper()=='Y' or 'Y' in add.upper():
-        while add.upper()!='DONE' and units<=maxcred:
-            add=input('\nType course number  (type DONE when finished):')
-            if add.upper()=='DONE':
-                break
-            elif add not in course_dict.keys():
-                print('Invalid course number! Try again')
-            elif add in courses:
-                print('current courses: '+str(courses))
-                print('You already have this class on your schedule. ')
-            elif units+course_dict[add]['units']>maxcred:
-                print('Adding this course would exceed your maximum units.')
-            else:
-                units+=course_dict[add]['units']
-                courses.append(add)
-                print('current courses: '+str(courses))
-                print('current units: '+str(units)+'/%d'%maxcred)
-    courses.sort()
-    return courses,units
 
 ###> Shailey's code:
 def coreClasses(major):
