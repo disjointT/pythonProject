844d74d84f4ea37328ccb938091a1d44905bbe8a
Anna Tan <annat@Annas-MacBook-Pro.local>
2020-11-30 02:22:24 -0500

    change addCourse() func

--- grabCore.py
+++ grabCore.py
@@ -43,21 +43,29 @@ def grabNumber():
     return major
 
 def addCourse(course_dict,maxcred=54):
-    add=input('Do you wanna add a class? (Y/N) :')
+    add=input('Do you wanna add a class? (Y/N): ')
     units=0
     courses=[]
+    print(courses)
     if add.upper()=='Y' or 'Y' in add.upper():
-        while add!='DONE' and units<=maxcred:
-            add=input('Type course number (type DONE when finished):')
-            if add not in course_dict.keys():
+        while add.upper()!='DONE' and units<=maxcred:
+            add=input('\nType course number  (type DONE when finished):')
+            if add.upper()=='DONE':
+                break
+            elif add not in course_dict.keys():
                 print('Invalid course number! Try again')
-                add=input('\nType course number  (type DONE when finished):')
+            elif add in courses:
+                print('current courses: '+str(courses))
+                print('You already have this class on your schedule. ')
             elif units+course_dict[add]['units']>maxcred:
                 print('Adding this course would exceed your maximum units.')
             else:
                 units+=course_dict[add]['units']
                 courses.append(add)
-    return course,units
+                print('current courses: '+str(courses))
+                print('current units: '+str(units)+'/%d'%maxcred)
+    courses.sort()
+    return courses,units
 
 ###> Shailey's code:
 def coreClasses(major):
@@ -107,11 +115,15 @@ def coreClasses(major):
         return msppmDACoreClasses
    ##<<
 
+
 def main():
-    #programs,hplink=program()
+    programs,hplink=program()
     major=grabNumber()
     print('Core:')
     core=coreClasses(major)
+    #test cases
+    #course_dict={'1':{'title':'abc','units':32,'description':""},'2':{'title':' df','units':22,'description':' '},'3':{'title':' d','units':2,'description':' '},'4':{'title':' d','units':12,'description':' '}}
+    #addCourse(course_dict)
 
 if __name__ == '__main__':
     main()
