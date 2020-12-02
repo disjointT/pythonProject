34c9d27f3cb3e404ec0a4332524446ff6cec5f5d
Anna Tan <annat@Annas-MacBook-Pro.local>
2020-12-02 04:21:07 -0500

    add course. write csv

--- addCourse.py
+++ addCourse.py
@@ -1,4 +1,22 @@
 import sys
+import time
+
+course_dict={}
+f=open('course_dict.txt')
+for line in f.readlines():
+    line=line.split('=')
+    val=line[1]
+    val=val.strip('{')
+    val=val.strip('}')
+    val=val.split('$')
+    vals={}
+    for item in val:
+        item=item.split(':')
+        if len(item)>1:
+            vals[item[0]]=item[1]
+            if item[0]=='Units':
+                vals[item[0]]=int(vals[item[0]])
+    course_dict[line[0]]=vals
 
 
 def addCourse(course_dict,maxcred=54):
@@ -8,7 +26,7 @@ def addCourse(course_dict,maxcred=54):
     print(courses)
     if add.upper()=='Y' or 'Y' in add.upper():
         while add.upper()!='DONE' and units<=maxcred:
-            add=input('\nType course number  (type DONE when finished):')
+            add=input('\nType course number XX-XXX (type DONE when finished):')
             if add.upper()=='DONE':
                 break
             elif add not in course_dict.keys():
@@ -16,20 +34,38 @@ def addCourse(course_dict,maxcred=54):
             elif add in courses:
                 print('current courses: '+str(courses))
                 print('You already have this class on your schedule. ')
-            elif units+course_dict[add]['units']>maxcred:
+            elif units+course_dict[add]['Units']>maxcred:
                 print('Adding this course would exceed your maximum units.')
             else:
-                units+=course_dict[add]['units']
-                courses.append(add)
-                print('current courses: '+str(courses))
-                print('current units: '+str(units)+'/%d'%maxcred)
+                print(course_dict[add])
+                adding=input('\nDo you want to add this class? Y/N ')
+                while adding.upper() not in ['Y','N']:
+                    if adding.upper()=='Y':
+                        units+=course_dict[add]['Units']
+                        courses.append(add)
+                        print('current courses: '+str(courses))
+                        print('current units: '+str(units)+'/%d'%maxcred)
+                    adding=input('Do you want to add this class? Y/N ')
+                if adding.upper()=='Y':
+                    units+=course_dict[add]['Units']
+                    courses.append(add)
+                    print('current courses: '+str(courses))
+                    print('current units: '+str(units)+'/%d'%maxcred)
+
     courses.sort()
-    return courses,units
+    return courses,units,maxcred
 
 def main():
-    #test cases
-    course_dict={'1':{'title':'abc','units':32,'description':""},'2':{'title':' df','units':22,'description':' '},'3':{'title':' d','units':2,'description':' '},'4':{'title':' d','units':12,'description':' '}}
-    addCourse(course_dict)
+    schedule,units,maxcred=addCourse(course_dict)
+    time.sleep(1)
+    print('generating schedule.................')
+    with open('schedule.csv','w') as f:
+        f.write(' ,total units, %d/%d\n'%(units,maxcred))
+        f.write('Course Number,Title,Units\n')
+        for c in schedule:
+            d=course_dict[c]
+            f.write('%s,%s,%d \n'%(c,d['Title'],d['Units']))
+
 
 if __name__ == '__main__':
     main()
